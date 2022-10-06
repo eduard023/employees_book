f11719e0c74cb463e93b09e3a475646c926eabe8
eduard023 <edo.avdzh@yandex.ru>
2022-10-06 23:30:22 +0300

    Основное задание

---
+++ src/Employee.java
@@ -0,0 +1,105 @@
+public class Employee {
+    private String fullName;
+    private int unit;
+    private int salary;
+    private int id;
+    private static int counter = 0;
+    public Employee(String fullName, int unit, int salary, int id) {
+        this.fullName = fullName;
+        this.unit = unit;
+        this.salary = salary;
+        this.id = counter++;
+
+    }
+
+    //Getters
+    public String getFullName() {
+        return this.fullName;
+    }
+
+    public int getUnit() {
+        return this.unit;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    //Setters
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return fullName + ", " + unit + " отдел. " + "Заработная плата: " + salary + " рублей. Id - " + id;
+    }
+    //вывод всех сотрудников
+    public static String printAllEmployee(Employee[] employees){
+        String fullInfo = "";
+        for (Employee employee: employees) {
+            fullInfo += employee.toString() + System.lineSeparator();
+        }
+        return fullInfo;
+    }
+    // Сумма затрат на зарплаты за месяц
+    public static String printSalaryCosts(Employee[] employees){
+        int sum = 0;
+        for (Employee salary: employees) {
+            sum += salary.salary;
+        }
+        return sum + " рублей.";
+    }
+    // Среднее значение зарплат
+    public static String printAverageSalary(Employee[] employees){
+        int sum = 0;
+        for (Employee salary: employees) {
+            sum += salary.salary;
+        }
+        return sum / employees.length + " рублей.";
+    }
+    // Поиск сотрудника с минимальной зарплатой
+    public static String searchEmployeeMinSalary(Employee[] employees){
+        int min = employees[0].salary;
+        String fio = employees[0].toString();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].salary < min){
+                min = employees[i].salary;
+                fio = employees[i].toString();
+            }
+        }
+        return fio;
+    }
+
+    //Поиск сотрудника с максимальной зарплатой
+    public static String searchEmployeeMaxSalary(Employee[] employees){
+        int max = employees[0].salary;
+        String fio = employees[0].toString();
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].salary > max){
+                max = employees[i].salary;
+                fio = employees[i].toString();
+            }
+        }
+        return fio;
+    }
+    //Вывод ФИО всех сотрудников
+    public static String printFullNameAllEmployees(Employee[] employees) {
+        String fullName1 = "";
+        for (Employee fullName : employees) {
+            fullName1 += fullName.fullName + System.lineSeparator();
+
+        }
+        return fullName1;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванов Петр Сергеевич", 1, 47900, 0);
+        employees[1] = new Employee("Сергеев Иван Александрович", 1, 40750, 0);
+        employees[2] = new Employee("Никифоров Никита Петрович", 2, 43900, 0);
+        employees[3] = new Employee("Дудик Артем Валерьевич", 2, 37500, 0);
+        employees[4] = new Employee("Сидоров Григорий Николаевич", 2, 35400, 0);
+        employees[5] = new Employee("Лаботовский Игнат Михайлович", 3, 46650, 0);
+        employees[6] = new Employee("Михайлов Станислав Иванович", 3, 48700, 0);
+        employees[7] = new Employee("Ибрагимов Михаил Вольфович", 4, 35640, 0);
+        employees[8] = new Employee("Петров Алексей Алексеевич", 4, 34900, 0);
+        employees[9] = new Employee("Якшин Тимур Кириллович", 5, 51000, 0);
+        System.out.println("Список всех сотрудников и данных по ним:\n" + Employee.printAllEmployee(employees));
+        System.out.println("Сумма затрат на зарплаты за месяц: " + Employee.printSalaryCosts(employees));
+        System.out.println("Сотрудник с минимальной заработной платой: " + Employee.searchEmployeeMinSalary(employees));
+        System.out.println("Сотрудник с максимальной заработной платой: " + Employee.searchEmployeeMaxSalary(employees));
+        System.out.println("Среднее значение зарплат: " + Employee.printAverageSalary(employees));
+        System.out.println("Список ФИО всех сотрудников:\n" + Employee.printFullNameAllEmployees(employees));
     }
+
 }
